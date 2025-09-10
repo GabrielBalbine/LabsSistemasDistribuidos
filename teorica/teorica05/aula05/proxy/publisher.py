5fbe9f4d5fee4e7fa0e6ad2eec39b814263d2e56
Gabriel Balbine <infinitybraw@gmail.com>
2025-09-10 23:15:12 +0000

    comitado

---
+++ teorica/teorica05/aula05/proxy/publisher.py
@@ -0,0 +1,14 @@
+import zmq
+from time import time, sleep
+
+context = zmq.Context()
+pub = context.socket(zmq.PUB)
+pub.connect("tcp://proxy:5555")
+
+while True:
+    message = str(time())
+    print(f"message: {message}", flush=True)
+    pub.send_string(message)
+
+pub.close()
+context.close()
